593fe63413f728d2e312742c0de8ce660ee96423
Ben Scott <gamepoet@gmail.com>
2012-03-07 00:31:30 -0800

    switch to a more sinatra-assetpack inspired api

--- lib/sinatra/ember.rb
+++ lib/sinatra/ember.rb
@@ -9,35 +9,74 @@ module Sinatra
     end
 
     module ClassMethods
-      # serves ember handlebars templates at the given path
-      def serve_templates(path)
-        get path do
-          content_type :js
+      # set ember options
+      def ember(&block)
+        @ember_options ||= Options.new(self, &block)
+        self.ember_init! if block_given?
+        @ember_options
+      end
 
-          # find all the template files
-          template_files = {}
-          Dir.chdir(settings.views) do
-            Dir['**/*.hbs'].map do |filename|
-              filename =~ /^(.*)\.hbs$/
-              name = $1
-              template_files[name] = File.join(settings.views, filename)
+      def ember_init!
+        ember.template_packages.each do |route, globs|
+          get route do
+            mtime, output = @template_cache.fetch(route) do
+              # find all the template files
+              paths = globs.map do |glob|
+                glob = File.expand_path(File.join(settings.root, glob))
+                Dir[glob].map { |x| x.squeeze('/') }
+              end
+              paths = paths.flatten.uniq
+
+              # build up template hash
+              template_paths = {}
+              paths.each do |path|
+                template_paths[File.basename(path, '.hbs')] = path
+              end
+
+              # build up the javascript
+              templates = template_paths.map do |(name, path)|
+                content = File.read(path)
+                "Ember.TEMPLATES[#{name.inspect}] = Ember.Handlebars.compile(#{content.inspect});"
+              end
+
+              # wrap it up in a closure
+              output = %{
+                (function() {
+                  #{templates.join("\n")}
+                })();
+              }
+              output = output.strip.gsub(/^ {16}/, '')
+
+              # compute the maximum mtime for all paths
+              mtime = paths.map do |path|
+                if File.file?(path)
+                  File.mtime(path).to_i
+                end
+              end.compact.max
+
+              [mtime, output]
             end
-          end
 
-          # build up the javascript
-          templates = template_files.map do |(name, file)|
-            content = File.read(file)
-            "Ember.TEMPLATES[#{name.inspect}] = Ember.Handlebars.compile(#{content.inspect});"
+            content_type :js
+            last_modified mtime
+            output
           end
-
-          # wrap it up in a closure
-          output = %{
-            (function() {
-              #{templates.join("\n")}
-            })();
-          }
-          output.strip.gsub(/^ {12}/, '')
         end
+      end
+    end
+
+    class Options
+      attr_reader :template_packages
+
+      def initialize(app, &block)
+        @app = app
+        @template_packages = {}
+
+        instance_eval(&block)
+      end
+
+      def templates(route, files=[])
+        @template_packages[route] = files
       end
     end
   end
